dbba3be51d5393eff6a506369a4b2a5ba5744d94
op0hac4u1 <p2s6626@naver.com>
2018-04-24 21:24:38 +0900

    ㅎㅎ
    ㅎㅎ

---
+++ aaa.rb
@@ -0,0 +1,10 @@
+aa = ["vejimil", "lemona", "chapsaldduk", "spicy ramun"].sort.reverse
+
+
+1.upto(3) do
+
+	aa.each do |bb|
+		puts bb.capitalize
+	end
+
+end
---
+++ server.rb
@@ -0,0 +1,8 @@
+#encoding:  UTF-8
+
+require 'sinatra'
+
+
+get '/join' do
+	"회원가입 페이지 입니다."
+end
